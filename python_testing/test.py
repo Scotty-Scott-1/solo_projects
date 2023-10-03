06846530698d2c26700d9c27c2a365a640c83d2c
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-03 16:31:14 +0200

    test

--- python_testing/test.py
+++ python_testing/test.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
-for i in range(4):
-    print(i, end=" ")
+a = {'id': 89, "name": "John", "list": [1, 2, 3, 4]}
+
+print("{}".format(a.get("fffffff")))
+
+for i in range(0, 3):
+    print(i)
