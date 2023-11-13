2257c3c377d9414fa568204bc40cf8ed79ce1ea5
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-11-13 09:39:17 +0100

    added files

---
+++ datingapp/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+	// JavaScript code to be executed after the page is fully loaded
+	alert("Welcome to the simple web app!");
+    });
+    
