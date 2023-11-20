9846f167e93442e24365dde9ec5f7be1a75b6bf8
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-11-20 20:51:25 +0100

    added files

--- datingapp/script.js
+++ datingapp/script.js
@@ -1,5 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-	// JavaScript code to be executed after the page is fully loaded
-	alert("Welcome to the simple web app!");
-    });
-    
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName("slide-container");
+  const dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "grid";
+  dots[slideIndex - 1].className += " active";
+
+  setTimeout(showSlides, 8000);
+}
